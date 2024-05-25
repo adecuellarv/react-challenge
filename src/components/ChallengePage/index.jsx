a16c58aaab687a2d5ebb858e26ed565ab996676f
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-05-24 22:38:21 -0600

    eslint

--- src/components/ChallengePage/index.jsx
+++ src/components/ChallengePage/index.jsx
@@ -3,15 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { setWord } from "../../store/actions/word";
 import { getWord } from "../../store/reselect";
 
-const ChallangePage = () => {
+function ChallangePage() {
     const dispatch = useDispatch();
     const word = useSelector(getWord);
-    
     return (
         <div>
             <button onClick={() => dispatch(setWord('ade'))}>Set word</button>
         </div>
     )
-};
+}
 
 export default ChallangePage
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
-import rootReducer from './reducer';
+import { configureStore } from "@reduxjs/toolkit";
+import { thunk } from "redux-thunk";
+import rootReducer from "./reducer";
 
 const store = configureStore({
   reducer: rootReducer,
